ff781964d6911211310e809a35af03346d39001f
Michael Gecht <gecht.m@gmail.com>
2018-03-11 13:16:32 +0100

    style: Undo a to button transformation

--- js/main.js
+++ js/main.js
@@ -156,7 +156,7 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
-  const more = document.createElement('button');
+  const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
