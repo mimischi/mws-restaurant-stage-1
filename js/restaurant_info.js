db75124dc9165f0b91fe584923f36353c0673162
Michael Gecht <gecht.m@gmail.com>
2018-03-11 15:39:47 +0100

    style: Clean up restaurant.html and make it mobile-first.

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -117,20 +117,31 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
  */
 createReviewHTML = (review) => {
   const li = document.createElement('li');
+  const head = document.createElement('p');
+  head.className = 'reviewer-date';
+  li.appendChild(head);
+  const headSelector = document.getElementsByClassName('reviewer-date')[0];
+
   const name = document.createElement('p');
+  head.appendChild(name);
   name.innerHTML = review.name;
-  li.appendChild(name);
+  name.className = 'reviewer';
+  // li.appendChild(name);
 
   const date = document.createElement('p');
+  head.appendChild(date);
   date.innerHTML = review.date;
-  li.appendChild(date);
+  date.className = 'date';
+  // li.appendChild(date);
 
   const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
+  rating.className = 'rating';
   li.appendChild(rating);
 
   const comments = document.createElement('p');
   comments.innerHTML = review.comments;
+  comments.className = 'comments';
   li.appendChild(comments);
 
   return li;
